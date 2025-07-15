0a7ba1faf41387280dc1586c3a6eac10ad69c2dd
Alauddeen <alauddeen.mohammad_consultant@jukshio.com>
2025-07-16 00:08:02 +0530

    Fix: removed duplicate Router declaration

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Home from "./Pages/Home";
 import Legal from "./Pages/Legal";
 import NotFound from "./Pages/NotFound";
 import Appointment from "./Pages/Appointment";
-import { HashRouter as Router } from "react-router-dom";
 
 function App() {
   return (
