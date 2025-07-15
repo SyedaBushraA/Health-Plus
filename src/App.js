e0c60db86d9b2ce3bd75f968211bf30ce009ce5e
Alauddeen <alauddeen.mohammad_consultant@jukshio.com>
2025-07-16 00:30:59 +0530

    Ready for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
 import Legal from "./Pages/Legal";
 import NotFound from "./Pages/NotFound";
 import Appointment from "./Pages/Appointment";
-import { HashRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
