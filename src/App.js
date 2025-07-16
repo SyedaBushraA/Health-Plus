91030ad9a6cfe4152609f3033e37f47dbb3c5f71
Alauddeen <alauddeen.mohammad_consultant@jukshio.com>
2025-07-16 11:33:16 +0530

    Ready for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Appointment from "./Pages/Appointment";
 function App() {
   return (
     <div className="App">
-      <Router basename="/TDS-Health-Plus">
+      <Router basename="/Health-Plus">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/legal" element={<Legal />} />
