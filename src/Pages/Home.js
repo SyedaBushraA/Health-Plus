bdff0dd230b9fff06c94a3c34283c349581d4250
Alauddeen <alauddeen.mohammad_consultant@jukshio.com>
2025-07-16 11:58:45 +0530

    Ready for GitHub Pages

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,8 +4,9 @@ import Hero from "../Components/Hero";
 import Info from "../Components/Info";
 import About from "../Components/About";
 import BookAppointment from "../Components/BookAppointment";
+import Reviews from "../Components/Reviews";
+import Doctors from "../Components/Doctors";
 import Footer from "../Components/Footer";
-import Contact from "../Components/Contact";
 
 function Home() {
   return (
@@ -15,7 +16,8 @@ function Home() {
       <Info />
       <About />
       <BookAppointment />
-      <Contact />
+      <Reviews />
+      <Doctors />
       <Footer />
     </div>
   );
